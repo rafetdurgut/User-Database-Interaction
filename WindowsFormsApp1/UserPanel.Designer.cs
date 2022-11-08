c8d0ba353603325175e83d34505c130e81832c3b
rafetdurgut <rafetdurgut@karabuk.edu.tr>
2022-11-08 18:18:31 +0300

    Proje dosyası ekle.

---
+++ WindowsFormsApp1/UserPanel.Designer.cs
@@ -0,0 +1,376 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class UserPanel
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.projectOneDataSet = new WindowsFormsApp1.ProjectOneDataSet();
+            this.uSERBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.uSERTableAdapter = new WindowsFormsApp1.ProjectOneDataSetTableAdapters.USERTableAdapter();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtUserName = new System.Windows.Forms.TextBox();
+            this.txtSurname = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnAddUser = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.txtUpdatePass = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtUpdateSurname = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtUpdateName = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtUpdateEmail = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.columnHeader5 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.projectOneDataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.uSERBindingSource)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // projectOneDataSet
+            // 
+            this.projectOneDataSet.DataSetName = "ProjectOneDataSet";
+            this.projectOneDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // uSERBindingSource
+            // 
+            this.uSERBindingSource.DataMember = "USER";
+            this.uSERBindingSource.DataSource = this.projectOneDataSet;
+            // 
+            // uSERTableAdapter
+            // 
+            this.uSERTableAdapter.ClearBeforeFill = true;
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3,
+            this.columnHeader5,
+            this.columnHeader4});
+            this.listView1.FullRowSelect = true;
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(12, 71);
+            this.listView1.MultiSelect = false;
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(839, 349);
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            this.listView1.SelectedIndexChanged += new System.EventHandler(this.listView1_SelectedIndexChanged);
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "User ID";
+            this.columnHeader1.Width = 104;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "User Name";
+            this.columnHeader2.Width = 105;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "Surname";
+            this.columnHeader3.Width = 194;
+            // 
+            // columnHeader4
+            // 
+            this.columnHeader4.Text = "Last Login";
+            this.columnHeader4.Width = 264;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.txtEmail);
+            this.groupBox1.Controls.Add(this.label8);
+            this.groupBox1.Controls.Add(this.btnAddUser);
+            this.groupBox1.Controls.Add(this.txtPassword);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.txtSurname);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.txtUserName);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(839, 53);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "User Information";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(7, 34);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(63, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "User Name:";
+            // 
+            // txtUserName
+            // 
+            this.txtUserName.Location = new System.Drawing.Point(77, 27);
+            this.txtUserName.Name = "txtUserName";
+            this.txtUserName.Size = new System.Drawing.Size(100, 20);
+            this.txtUserName.TabIndex = 1;
+            // 
+            // txtSurname
+            // 
+            this.txtSurname.Location = new System.Drawing.Point(258, 27);
+            this.txtSurname.Name = "txtSurname";
+            this.txtSurname.Size = new System.Drawing.Size(100, 20);
+            this.txtSurname.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(188, 34);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(49, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Surname";
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(439, 27);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(100, 20);
+            this.txtPassword.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(369, 34);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(56, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Password:";
+            // 
+            // btnAddUser
+            // 
+            this.btnAddUser.Location = new System.Drawing.Point(758, 19);
+            this.btnAddUser.Name = "btnAddUser";
+            this.btnAddUser.Size = new System.Drawing.Size(75, 23);
+            this.btnAddUser.TabIndex = 8;
+            this.btnAddUser.Text = "Add User";
+            this.btnAddUser.UseVisualStyleBackColor = true;
+            this.btnAddUser.Click += new System.EventHandler(this.btnAddUser_Click);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Maroon;
+            this.button1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button1.Location = new System.Drawing.Point(693, 426);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(158, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Delete Selected User(s)";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.txtUpdateEmail);
+            this.groupBox2.Controls.Add(this.label7);
+            this.groupBox2.Controls.Add(this.button2);
+            this.groupBox2.Controls.Add(this.txtUpdatePass);
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Controls.Add(this.txtUpdateSurname);
+            this.groupBox2.Controls.Add(this.label5);
+            this.groupBox2.Controls.Add(this.txtUpdateName);
+            this.groupBox2.Controls.Add(this.label6);
+            this.groupBox2.Location = new System.Drawing.Point(12, 464);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(839, 53);
+            this.groupBox2.TabIndex = 9;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "User Information";
+            this.groupBox2.Visible = false;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(758, 25);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "Update User";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // txtUpdatePass
+            // 
+            this.txtUpdatePass.Location = new System.Drawing.Point(439, 27);
+            this.txtUpdatePass.Name = "txtUpdatePass";
+            this.txtUpdatePass.Size = new System.Drawing.Size(100, 20);
+            this.txtUpdatePass.TabIndex = 5;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(369, 34);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(56, 13);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Password:";
+            // 
+            // txtUpdateSurname
+            // 
+            this.txtUpdateSurname.Location = new System.Drawing.Point(258, 27);
+            this.txtUpdateSurname.Name = "txtUpdateSurname";
+            this.txtUpdateSurname.Size = new System.Drawing.Size(100, 20);
+            this.txtUpdateSurname.TabIndex = 3;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(188, 34);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(49, 13);
+            this.label5.TabIndex = 2;
+            this.label5.Text = "Surname";
+            // 
+            // txtUpdateName
+            // 
+            this.txtUpdateName.Location = new System.Drawing.Point(77, 27);
+            this.txtUpdateName.Name = "txtUpdateName";
+            this.txtUpdateName.Size = new System.Drawing.Size(100, 20);
+            this.txtUpdateName.TabIndex = 1;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(7, 34);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(63, 13);
+            this.label6.TabIndex = 0;
+            this.label6.Text = "User Name:";
+            // 
+            // txtUpdateEmail
+            // 
+            this.txtUpdateEmail.Location = new System.Drawing.Point(627, 25);
+            this.txtUpdateEmail.Name = "txtUpdateEmail";
+            this.txtUpdateEmail.Size = new System.Drawing.Size(100, 20);
+            this.txtUpdateEmail.TabIndex = 10;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(557, 32);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(36, 13);
+            this.label7.TabIndex = 9;
+            this.label7.Text = "E-Mail";
+            // 
+            // columnHeader5
+            // 
+            this.columnHeader5.Text = "Email";
+            this.columnHeader5.Width = 164;
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Location = new System.Drawing.Point(627, 27);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(100, 20);
+            this.txtEmail.TabIndex = 10;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(585, 32);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(36, 13);
+            this.label8.TabIndex = 9;
+            this.label8.Text = "E-Mail";
+            // 
+            // UserPanel
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(863, 529);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.listView1);
+            this.Name = "UserPanel";
+            this.Text = "UserPanel";
+            ((System.ComponentModel.ISupportInitialize)(this.projectOneDataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.uSERBindingSource)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private ProjectOneDataSet projectOneDataSet;
+        private System.Windows.Forms.BindingSource uSERBindingSource;
+        private ProjectOneDataSetTableAdapters.USERTableAdapter uSERTableAdapter;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.ColumnHeader columnHeader4;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnAddUser;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtSurname;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtUserName;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.TextBox txtUpdateEmail;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox txtUpdatePass;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtUpdateSurname;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtUpdateName;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.ColumnHeader columnHeader5;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.Label label8;
+    }
+}
---
+++ WindowsFormsApp1/UserPanel.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class UserPanel : Form
+    {
+        public static string connectionString = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=ProjectOne;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+        public SqlConnection sqlConnection = new SqlConnection(connectionString);
+        public UserPanel()
+        {
+            InitializeComponent();
+            LoadFromDatabase();
+            
+        }
+
+        private void LoadFromDatabase()
+        {
+            try
+            {
+                listView1.Items.Clear();
+                sqlConnection.Open();
+                SqlCommand cmd = new SqlCommand("SELECT * FROM dbo.[USER]", sqlConnection);
+                SqlDataReader dr = cmd.ExecuteReader();
+                while (dr.Read())
+                {
+                    ListViewItem li = new ListViewItem(dr["Id"].ToString());
+                    li.SubItems.Add(dr["Name"].ToString());
+                    li.SubItems.Add(dr["Surname"].ToString());
+                    li.SubItems.Add(dr["Email"].ToString());
+                    li.SubItems.Add(dr["LastLoginDate"].ToString());
+
+                    listView1.Items.Add(li);
+                }
+                sqlConnection.Close();
+            }
+            catch (Exception e)
+            {
+                sqlConnection.Close();
+
+                MessageBox.Show(e.Message);
+            }
+        }
+
+        private void btnAddUser_Click(object sender, EventArgs e)
+        {
+            if(string.IsNullOrEmpty(txtUserName.Text) || string.IsNullOrEmpty(txtSurname.Text) || string.IsNullOrEmpty(txtPassword.Text))
+            {
+                return;
+            }
+
+            SqlCommand sqlCommand = new SqlCommand("INSERT INTO dbo.[USER] (Name, Surname, Password, Email, LastLoginDate) " +
+                "VALUES('" + txtUserName.Text + "', '" + txtSurname.Text + "', '" + txtPassword.Text + "', '" + txtPassword.Text + "' ,'" + DateTime.Now.ToString("s") + "')", sqlConnection);
+            try
+            {
+
+                sqlConnection.Open();
+                sqlCommand.ExecuteNonQuery();
+                sqlConnection.Close();
+                LoadFromDatabase();
+            }
+            catch (SqlException ex)
+            {
+
+                sqlConnection.Close();
+                MessageBox.Show(ex.Message);
+            }
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            foreach (ListViewItem item in listView1.Items)
+            {
+                if(item.Selected)
+                {
+                    SqlCommand sqlCommand = new SqlCommand("DELETE FROM dbo.[USER] WHERE Id = " + item.Text, sqlConnection);
+                    try
+                    {
+
+                        sqlConnection.Open();
+                        sqlCommand.ExecuteNonQuery();
+                        sqlConnection.Close();
+                        LoadFromDatabase();
+                    }
+                    catch (SqlException ex)
+                    {
+
+                        sqlConnection.Close();
+                        MessageBox.Show(ex.Message);
+                    }
+                }
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(txtUpdateEmail.Text) || string.IsNullOrEmpty(txtUpdateName.Text) || string.IsNullOrEmpty(txtUpdateSurname.Text))
+            {
+                return;
+            }
+
+            SqlCommand sqlCommand = new SqlCommand("UPDATE dbo.[USER] SET Name=@name, Surname=@surname, Email=@email, Password=@password WHERE Id="+ listView1.SelectedItems[0].Text, sqlConnection);
+            sqlCommand.Parameters.AddWithValue("@name",txtUpdateName.Text);
+            sqlCommand.Parameters.AddWithValue("@surname", txtUpdateSurname.Text);
+            sqlCommand.Parameters.AddWithValue("@email", txtUpdateEmail.Text);
+            sqlCommand.Parameters.AddWithValue("@password", txtUpdatePass.Text);
+
+
+            try
+            {
+
+                sqlConnection.Open();
+                sqlCommand.ExecuteNonQuery();
+                sqlConnection.Close();
+                LoadFromDatabase();
+            }
+            catch (SqlException ex)
+            {
+
+                sqlConnection.Close();
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if(listView1.SelectedItems.Count > 0)
+            {
+                groupBox2.Show();
+                string id = listView1.SelectedItems[0].Text;
+
+                try
+                {
+                    sqlConnection.Open();
+                    SqlCommand cmd = new SqlCommand("SELECT * FROM dbo.[USER] WHERE Id = "+ id + "", sqlConnection);
+                    SqlDataReader dr = cmd.ExecuteReader();
+                    while (dr.Read())
+                    {
+                        txtUpdateEmail.Text = dr["Email"].ToString();
+                        txtUpdateName.Text = dr["Name"].ToString();
+                        txtUpdateSurname.Text = dr["Surname"].ToString();
+                        txtUpdatePass.Text = dr["Password"].ToString();
+                    }
+                    sqlConnection.Close();
+                }
+                catch (Exception ee)
+                {
+                    sqlConnection.Close();
+
+                    MessageBox.Show(ee.Message);
+                }
+            }
+            else
+            {
+                groupBox2.Hide();
+            }
+        }
+    }
+}
